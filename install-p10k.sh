a040a43d0b26c2513ed2e679b6bbb634a0b213b8
Niels Wind <nhwind@gmail.com>
2020-09-05 17:05:52 +0200

    add p10k install script

---
+++ install-p10k.sh
@@ -0,0 +1,4 @@
+# install font:
+# https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
